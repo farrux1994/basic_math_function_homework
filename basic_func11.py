8a89ccc578bc9cd95a2265e6a6a14bff4f7a14a2
farrux1994 <f.turdiyevbek94@gmail.com>
2022-12-27 00:10:13 +0500

    basic_func11

--- basic_func11.py
+++ basic_func11.py
@@ -1,10 +1,5 @@
 def main(a):
-    '''find the absolute value of a and return it
-    
-    Args:
-        a (int): a number
-        
-    Returns:
-        int: the absolute value.
-    '''
-    return
+    x = a
+    return x
+y= main(2)
+print(y)
